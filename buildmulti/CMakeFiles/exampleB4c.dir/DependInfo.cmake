cc68b807b1f3692a0d707dd229a24b98c2945854
Omrir52 <118667580+Omrir52@users.noreply.github.com>
2023-04-15 02:05:56 +0100

    Add files via upload

---
+++ buildmulti/CMakeFiles/exampleB4c.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/exampleB4c.cc" "CMakeFiles/exampleB4c.dir/exampleB4c.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/exampleB4c.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/ActionInitialization.cc" "CMakeFiles/exampleB4c.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/ActionInitialization.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/CalorHit.cc" "CMakeFiles/exampleB4c.dir/src/CalorHit.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/CalorHit.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/CalorimeterSD.cc" "CMakeFiles/exampleB4c.dir/src/CalorimeterSD.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/CalorimeterSD.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/DetectorConstruction.cc" "CMakeFiles/exampleB4c.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/DetectorConstruction.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/EventAction.cc" "CMakeFiles/exampleB4c.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/EventAction.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleB4c.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/RunAction.cc" "CMakeFiles/exampleB4c.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/RunAction.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/detector.cc" "CMakeFiles/exampleB4c.dir/src/detector.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/detector.cc.o.d"
+  "/home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/run.cc" "CMakeFiles/exampleB4c.dir/src/run.cc.o" "gcc" "CMakeFiles/exampleB4c.dir/src/run.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ buildmulti/CMakeFiles/exampleB4c.dir/src/detector.cc.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/exampleB4c.dir/src/detector.cc.o: \
+ /home/omrir52/software/geant4/geant4-v11.1.0/WbQD-Scint/src/detector.cc \
+ /usr/include/stdc-predef.h
